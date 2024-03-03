029317cba22bd44deb2b854ab425267bc34db66f
divinekaremera <m.karemera@alustudent.com>
2024-03-03 12:41:34 -0800

    first done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Function that fetches number_of_subscribers"""
-    URL = f"https://www.reddit.com/r/{subreddit}/about.json"
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-
-    try:
-        response = requests.get(URL, headers=HEADERS, allow_redirects=False)
-
-        # Check if the status code indicates success (200)
-        if response.status_code == 200:
-            return response.json().get("data").get("subscribers")
-        elif response.status_code == 404:
-            return f"The subreddit '{subreddit}' does not exist."
-        else:
-            return "Failed to retrieve subscriber count."
-
-    except Exception as e:
-        print("An error occurred:", e)
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
-# Test with existing and non-existing subreddits
-existing_subreddit = "python"
-non_existing_subreddit = "nonexistingsubreddit123"
-
-print("Number of subscribers in the existing subreddit:", number_of_subscribers(existing_subreddit))
-print("Number of subscribers in the non-existing subreddit:", number_of_subscribers(non_existing_subreddit))
-
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
